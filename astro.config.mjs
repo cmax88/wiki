cbab554d305148ea72a372a84e7acd3a7403f0f1
cmax88 <cjmax88@gmail.com>
2024-03-24 17:35:18 -0400

    added 3d model finally

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
-
 import vue from "@astrojs/vue";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
-  integrations: [tailwind(), icon(), vue()]
+  integrations: [tailwind(), icon(), vue(), react()]
 });
-
